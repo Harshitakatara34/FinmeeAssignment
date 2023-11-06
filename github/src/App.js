8b7a4e609fead119924e1eb50bac20719177b5b2
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-11-06 22:35:22 +0530

    d

--- github/src/App.js
+++ github/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import Section1 from './Components/Section1';
+// import Section2 from "./Components/Section2"
 function App() {
   return (
     <div className="hero">
     <Header/>
+    <Section1/>
+    {/* <Section2/> */}
     </div>
   );
 }
